f40ac3247fc866cde9d353fa8cd2a662572f2e08
mariahclaugesen <mariahl2@illinois.edu>
2019-04-22 20:13:20 -0500

    Made answer-setting pages.

---
+++ app/src/main/java/com/example/self_study_app/SetAnswerNumber.java
@@ -0,0 +1,13 @@
+package com.example.self_study_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetAnswerNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_answer_number);
+    }
+}
